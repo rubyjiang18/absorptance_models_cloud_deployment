6910c0d6eac94f5f8470de816a6e090360f9a08e
Runbo Jiang <runboj@andrew.cmu.edu>
2023-04-28 14:52:25 -0400

    Two approaches all works now

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask, request, jsonify, render_template, url_for, abort
 import pickle
+import imghdr
+import torch
+import torch.nn as nn
+from torchvision.models import convnext_tiny
+from PIL import Image
+from PIL import Image
+
+from torchvision import transforms
+from torchvision.transforms import ToTensor
 
 
 app = Flask(__name__)
-model = pickle.load(open('rf_absorptance_model.pkl','rb'))
+
+# the random forest model
+rf_model = pickle.load(open('rf_absorptance_model.pkl','rb'))
+# the convnext deep learning model
+dl_model  = convnext_tiny(weights=None)
+dl_model.classifier[2] = nn.Linear(768, 1)
+checkpoint = torch.load("dl_absorptance_weights", map_location=torch.device('cpu'))
+dl_model.load_state_dict(checkpoint['model_state_dict'])
+device = 'cpu'
+dl_model.to(device)
+
+test_preprocess =  transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
 
 @app.route('/')
@@ -12,8 +36,8 @@ def home():
     #return 'Hello World'
     return render_template('home.html')
 
-@app.route('/predict',methods = ['POST'])
-def predict():
+@app.route('/rf_predict',methods = ['POST'])
+def rf_predict():
     int_features = [float(x) for x in request.form.values()]
     # organize features
     depth, width_half, area = int_features[0], int_features[1], int_features[2]
@@ -21,20 +45,53 @@ def predict():
     int_features = [depth, width_half,aspect_ratio, area]
 
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    print(prediction[0])
+    prediction = rf_model.predict(final_features)
+
     return render_template('home.html', prediction_text="Laser Energy Absorptance {}%".format(round(prediction[0],2)))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+def validate_image(image):
+    # Check that the file extension is .tif
+    if not image.filename.endswith('.tif'):
+        abort(400, 'File must be in .tif format')
+    # Check that the file is a valid TIFF image
+    if imghdr.what(image) != 'tiff':
+        abort(400, 'File is not a valid TIFF image')
+    # Check that the image is a gray scale image
+    image = Image.open(image)
+    channels = image.getbands()
+    if len(channels) != 1: 
+        abort(400, 'File has to a gray scale tif image')
 
-    output = prediction[0]
-    return jsonify(output)
+def preprocess_image(image):
+    image = np.array(Image.open(image))
+    image = torch.tensor(image,dtype=torch.float64).unsqueeze_(0)
+    image = image.repeat(3, 1, 1)
+    return image
+
+@app.route('/dl_predict',methods = ['POST'])
+def dl_predict():
+    image = request.files['image']
+    validate_image(image)
+    # preprocess iamge
+    image = preprocess_image(image)
+    image = image.unsqueeze(0)
+    image = image.float().to(device)
+    # model prediction
+    output = dl_model(image) 
+    output= float(output.squeeze(1).detach().numpy()[0])
+    #
+    return render_template('home.html', prediction_text="Laser Energy Absorptance {}%".format(round(output,2)))
+
+# @app.route('/predict_api',methods=['POST'])
+# def predict_api():
+#     '''
+#     For direct API calls trought request
+#     '''
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
+
+#     output = prediction[0]
+#     return jsonify(output)
 
 
 
